b2ec1621329fbc5cb114ab90c870bab6e04c616f
joncoe <info@joncoe.com>
2023-07-22 16:17:04 -0400

    WIP add fields, button, styles

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 const ColorPaletteModal = () => {
+  const [name, setName] = useState('');
+  const onChangeText = e => {
+    console.log(e);
+  };
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={text => setName(text)}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  container: {
+    padding: 5,
+    paddingTop: 15,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  button: {
+    height: 40,
+    backgroundColor: 'teal',
+    borderRadius: 19,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
 export default ColorPaletteModal;
