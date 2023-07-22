a5837f664d7b6dc4f8d6fdd786b46ee6e9cd5619
joncoe <info@joncoe.com>
2023-07-22 09:42:56 -0400

    Port the current App component code to the new ColorPalette component

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
+import ColorBox from '../components/ColorBox';
 
 const ColorPalette = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <FlatList
+          data={COLORS}
+          renderItem={({ item }) => (
+            <ColorBox hexCode={item.hexCode} colorName={item.colorName} />
+          )}
+          ListHeaderComponent={
+            <Text style={styles.heading}>
+              Here are some boxes of different colours
+            </Text>
+          }
+          keyExtractor={item => item.hexCode}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'black',
+  },
+});
 
 export default ColorPalette;
