ba4c9ea393e8f539666e0c482a1f18646cbd9ac1
joncoe <info@joncoe.com>
2023-07-22 10:20:06 -0400

    Hook up navigation to the  color screen. Make the application Japanese, just for fun

--- App.tsx
+++ App.tsx
@@ -1,62 +1,29 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
-import ColorBox from './components/ColorBox';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import ColorPalette from './screens/ColorPalette';
 
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <View style={styles.container}>
-          <FlatList
-            data={COLORS}
-            renderItem={({ item }) => (
-              <ColorBox hexCode={item.hexCode} colorName={item.colorName} />
-            )}
-            ListHeaderComponent={
-              <Text style={styles.heading}>
-                Here are some boxes of different colours
-              </Text>
-            }
-            keyExtractor={item => item.hexCode}
-          />
-        </View>
-      </SafeAreaView>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'ホーム 🏠' }}
+        />
+        <Stack.Screen
+          name="ColorPalette"
+          component={ColorPalette}
+          options={{ title: '🌈 色パーレット 🌈' }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-    paddingHorizontal: 10,
-  },
-  heading: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color: 'black',
-  },
-});
 
 export default App;
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
-const Home = () => {
+const Home = ({ navigation: { navigate } }) => {
   return (
     <View>
-      <Text>Hello World</Text>
+      <TouchableOpacity onPress={() => navigate('ColorPalette')}>
+        <Text>ソラリゼーションダーク</Text>
+      </TouchableOpacity>
     </View>
   );
 };
